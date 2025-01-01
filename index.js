cfcaf02146eca6b7a71991512dfe31d495331394
Miguel Nazario <95545880+gitMiguel27@users.noreply.github.com>
2025-01-01 17:41:40 -0500

    fetch is working to grab the book cover and title and display a list to user

--- index.js
+++ index.js
@@ -2,24 +2,62 @@ import { fetchHeader } from "./headers.js";
 
 const body = document.querySelector('body');
 const form = document.getElementById('search-form');
+const input = document.getElementById('form-input');
+const listContainerDiv = document.createElement('div');
+listContainerDiv.classList.add('list-group');
+
 
 form.addEventListener('submit', (event) => {
     event.preventDefault();
-    getLord();
+
+    const search = input.value.replace(/\s/g, "+");
+    body.appendChild(listContainerDiv);
+    while(listContainerDiv.firstChild) {
+        listContainerDiv.removeChild(listContainerDiv.lastChild);
+    };
+    getSearch(search);
 });
 
-async function getLord() {
+async function getSearch(title) {
     try {
-        const lord = await fetch('https://openlibrary.org/search.json?title=the+lord+of+the+rings', {
+        const books = await fetch(`https://openlibrary.org/search.json?title=${title}`, {
             headers: fetchHeader
         });
-        const lordData = await lord.json();
-        console.log(lordData.docs);
-        // console.log(lordData.docs[0].title);
+        const booksData = await books.json();
+        const filteredData = booksData.docs.filter((book) => book.cover_i);
+
+        let iterate = 0;
+        while (iterate <= 10) {
+            // console.log(filteredData[iterate]);
+            const optionDiv = document.createElement('div');
+            optionDiv.classList.add('d-flex', 'justify-content-between');
+
+            const coverImg = getCoverImage(filteredData[iterate]);
+
+            const coverOption = document.createElement('button');
+            coverOption.classList.add('list-group-item', 'list-group-item-action');
+            coverOption.type = 'button';
+            coverOption.textContent = filteredData[iterate].title;
+
+            optionDiv.appendChild(coverImg);
+            optionDiv.appendChild(coverOption);
+            listContainerDiv.appendChild(optionDiv);
+            iterate++;
+        };
+        iterate = 0;
     } catch (error) {
         console.error({ error: error.message });
     };
+};
+
+function getCoverImage(option) {
     try {
+        const coverImg = document.createElement('img');
+        coverImg.src = `https://covers.openlibrary.org/b/id/${option.cover_i}-S.jpg`;
+        coverImg.alt = `${option.title}`;
+
+        return coverImg;
+
         //* FETCHING COVER JSON DATA METHOD:
         // const cover = await fetch('https://covers.openlibrary.org/b/id/14625765.json', {
         //     headers: fetchHeader
@@ -30,10 +68,6 @@ async function getLord() {
         // coverImg.src = coverData.source_url;
 
         // body.appendChild(coverImg);
-        const coverImg = document.createElement('img');
-        coverImg.src = 'https://covers.openlibrary.org/b/id/240727-S.jpg';
-
-        body.appendChild(coverImg);
     } catch (error) {
         console.error({ error: error.message });
     };
